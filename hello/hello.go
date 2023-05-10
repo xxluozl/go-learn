7cb5022e0410cfacbaecf14e72855e910389c9d3
luozl <2247735182@qq.com>
2023-05-10 15:52:18 +0800

    Add a test

--- hello/hello.go
+++ hello/hello.go
@@ -11,10 +11,11 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greet.Hello("Luo")
+	names := []string{"Luo", "Zhong", "Lin"}
+	messages, err := greet.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
